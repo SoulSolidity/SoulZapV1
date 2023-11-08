874522e6f9e76ed5095240d0cc364be491407fc3
defifofum <defifofum0@gmail.com>
2023-11-07 22:39:51 -0600

    refactor: dry up ApeBond extension - [wip] current ApeBond lens needs work, can be dry-ed up a lot too

--- contracts/extensions/ApeBond/ApeBond.sol
+++ contracts/extensions/ApeBond/ApeBond.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "../../SoulZap.sol";
-import "./lib/ICustomBillRefillable.sol";
-import "../../lib/IApeRouter02.sol";
-import "../../lib/ISoulZap.sol";
-import "../../SoulFee.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-abstract contract ApeBond is SoulZap {
-    event ZapBond(ZapParamsBond zapParamsBond);
-    event ZapBondNative(ZapParamsBondNative zapParamsBondNative);
+import {SoulZap_UniV2} from "../../SoulZap.sol";
+import "./lib/ICustomBillRefillable.sol";
+// TODO: Use official Uniswap Interfaces
+import "../../lib/IApeRouter02.sol";
+import "../../lib/IApePair.sol";
+
+// TODO: Need this?
+// import {ISoulFeeManager} from "../../fee-manager/ISoulFeeManager.sol";
+
+// TODO: Update name to SoulZap_ApeBond.sol
+abstract contract SoulZapExt_ApeBond is SoulZap_UniV2 {
+    event ZapBond(ZapParams zapParams, ICustomBillRefillable bill, uint256 maxPrice);
+    event ZapBondNative(ZapParams zapParams, ICustomBillRefillable bill, uint256 maxPrice);
+    // TODO: Pausing may be overkill for extensions, but it could be cool to have a feature based pause mechanism in SoulZap
+    // ex: `const APE_BOND_ZAP_UNIV2_FEATURE = APE_BOND_ZAP_UNIV2`
     bool public apeBondPaused = false;
 
     constructor() {}
@@ -19,71 +27,75 @@ abstract contract ApeBond is SoulZap {
         _;
     }
 
-    function pauseApeBond() public onlyOwner {
+    function pauseApeBond() public restricted {
         apeBondPaused = true;
     }
 
-    function unpauseApeBond() public onlyOwner {
+    function unpauseApeBond() public restricted {
         apeBondPaused = false;
     }
 
-    /// @notice Zap single token to LP
-    /// @param zapParamsBond all parameters for Bond zap
-    /// inputToken Input token to zap
-    /// inputAmount Amount of input tokens to zap
-    /// underlyingTokens Tokens of LP to zap to
-    /// paths Path from input token to LP token0
-    /// minAmounts The minimum amount of output tokens that must be received for
-    ///   swap and AmountAMin and amountBMin for adding liquidity
-    /// deadline Unix timestamp after which the transaction will revert
-    /// bill Treasury bill address
-    /// maxPrice Max price of treasury bill
-    function zapBond(ZapParamsBond memory zapParamsBond) external nonReentrant whenNotPausedApeBond {
-        IApePair pair = IApePair(zapParamsBond.bill.principalToken());
-        require(
-            (zapParamsBond.zapParams.token0 == pair.token0() && zapParamsBond.zapParams.token1 == pair.token1()) ||
-                (zapParamsBond.zapParams.token1 == pair.token0() && zapParamsBond.zapParams.token0 == pair.token1()),
-            "ApeBond: Wrong LP pair for Bond"
-        );
-        address to = zapParamsBond.zapParams.to;
-        zapParamsBond.zapParams.to = address(this);
-        _zapInternal(zapParamsBond.zapParams, soulFee.getFee("apebond-bond-zap"));
-
-        uint256 balance = pair.balanceOf(address(this));
-        pair.approve(address(zapParamsBond.bill), balance);
-        zapParamsBond.bill.deposit(balance, zapParamsBond.maxPrice, to);
-        pair.approve(address(zapParamsBond.bill), 0);
-        emit ZapBond(zapParamsBond);
+    /// @notice Zap single token to ApeBond
+    /// @param zapParams ISoulZap.ZapParams
+    /// @param bill Treasury bill address
+    /// @param maxPrice Max price of treasury bill
+    function zapBond(
+        ZapParams memory zapParams,
+        // TODO: Rebrand to `IApeBond bond`?
+        ICustomBillRefillable bill,
+        uint256 maxPrice
+    ) external nonReentrant whenNotPausedApeBond {
+        _zapBond(zapParams, false, bill, maxPrice);
     }
 
     /// @notice Zap native token to Treasury Bill
-    /// @param zapParamsBondNative all parameters for native Bond zap
-    /// underlyingTokens Tokens of LP to zap to
-    /// paths Path from input token to LP token0
-    /// minAmounts The minimum amount of output tokens that must be received for
-    ///   swap and AmountAMin and amountBMin for adding liquidity
-    /// deadline Unix timestamp after which the transaction will revert
-    /// bill Treasury bill address
-    /// maxPrice Max price of treasury bill
+    /// @param zapParamsNative ISoulZap.ZapParamsNative
+    /// @param bill Treasury bill address
+    /// @param maxPrice Max price of treasury bill
     function zapBondNative(
-        ZapParamsBondNative memory zapParamsBondNative
+        ZapParamsNative memory zapParamsNative,
+        ICustomBillRefillable bill,
+        uint256 maxPrice
     ) external payable nonReentrant whenNotPausedApeBond {
-        IApePair pair = IApePair(zapParamsBondNative.bill.principalToken());
+        (IERC20 wNative, uint256 inputAmount) = _wrapNative();
+
+        ZapParams memory zapParams = ZapParams({
+            inputToken: wNative,
+            inputAmount: inputAmount,
+            token0: zapParamsNative.token0,
+            token1: zapParamsNative.token1,
+            path0: zapParamsNative.path0,
+            path1: zapParamsNative.path1,
+            liquidityPath: zapParamsNative.liquidityPath,
+            to: zapParamsNative.to,
+            deadline: zapParamsNative.deadline
+        });
+
+        _zapBond(zapParams, true, bill, maxPrice);
+    }
+
+    function _zapBond(ZapParams memory zapParams, bool native, ICustomBillRefillable bill, uint256 maxPrice) private {
+        IApePair pair = IApePair(bill.principalToken());
+        // TODO: Will need to expand this because bonds can technically be any token, not just LPs
         require(
-            (zapParamsBondNative.zapParamsNative.token0 == pair.token0() &&
-                zapParamsBondNative.zapParamsNative.token1 == pair.token1()) ||
-                (zapParamsBondNative.zapParamsNative.token1 == pair.token0() &&
-                    zapParamsBondNative.zapParamsNative.token0 == pair.token1()),
+            (zapParams.token0 == pair.token0() && zapParams.token1 == pair.token1()) ||
+                (zapParams.token1 == pair.token0() && zapParams.token0 == pair.token1()),
             "ApeBond: Wrong LP pair for Bond"
         );
-        address to = zapParamsBondNative.zapParamsNative.to;
-        zapParamsBondNative.zapParamsNative.to = address(this);
-        _zapNativeInternal(zapParamsBondNative.zapParamsNative, soulFee.getFee("apebond-bond-zap"));
+        address to = zapParams.to;
+        zapParams.to = address(this);
+        // TODO: getEpochVolume() is experimental. Volume is currently not yet tracked
+        _zap(zapParams, native, soulFeeManager.getFee(getEpochVolume()));
 
         uint256 balance = pair.balanceOf(address(this));
-        pair.approve(address(zapParamsBondNative.bill), balance);
-        zapParamsBondNative.bill.deposit(balance, zapParamsBondNative.maxPrice, to);
-        pair.approve(address(zapParamsBondNative.bill), 0);
-        emit ZapBondNative(zapParamsBondNative);
+        pair.approve(address(bill), balance);
+        bill.deposit(balance, maxPrice, to);
+        pair.approve(address(bill), 0);
+
+        if (native) {
+            emit ZapBondNative(zapParams, bill, maxPrice);
+        } else {
+            emit ZapBond(zapParams, bill, maxPrice);
+        }
     }
 }
