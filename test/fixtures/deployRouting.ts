381708739cc88c7e9d423a5e962dc07ed6212950
defifofum <defifofum0@gmail.com>
2023-11-14 21:06:38 -0600

    fix: Deployment scripts due to contract update

--- test/fixtures/deployRouting.ts
+++ test/fixtures/deployRouting.ts
@@ -3,14 +3,13 @@ import { getDeployConfig, DeployableNetworks } from '../../scripts/deploy/deploy
 import { ZERO_ADDRESS } from '../../src'
 export async function deployRoutingFixture(_ethers: typeof ethers, chain: DeployableNetworks, souZapAddress: string, router: string) {
   // Contracts are deployed using the first signer/account by default
-  const { wNative, admin, dexInfo, hopTokens, feeCollector, protocolFee, proxyAdminAddress, maxFee } = getDeployConfig(chain)
+  const { wNative, admin, dexInfo, feeCollector, protocolFee, proxyAdminAddress, maxFee } = getDeployConfig(chain)
   const [owner, otherAccount] = await _ethers.getSigners()
+  const hopTokens = dexInfo.ApeBond?.hopTokens || []
 
   const SoulZap_Lens = await _ethers.getContractFactory('SoulZap_UniV2_Extended_Lens_V1')
   const soulZap_Lens = await SoulZap_Lens.deploy(
     souZapAddress,
-    wNative,
-    hopTokens[0],
     router,
     hopTokens
   )
