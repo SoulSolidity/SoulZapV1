2303de0eabc890f1dc3ea3e94e386af1f4b7788d
defifofum <defifofum0@gmail.com>
2023-12-12 15:31:43 -0600

    feat(deploy): Write deployment outputs to file, provide deploy script for SoulAccessRegistry

--- scripts/deploy/deploy.config.ts
+++ scripts/deploy/deploy.config.ts
@@ -1,6 +1,8 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { Networks } from '../../hardhat'
 import path from 'path'
+import { ether } from '../../test/utils'
+import { BigNumberish } from 'ethers'
 
 // Define a base directory for deployments
 export const DEPLOYMENTS_BASE_DIR = path.resolve(__dirname, '../../deployments')
@@ -23,7 +25,7 @@ export const getDeployConfig = (network: DeployableNetworks, signers?: SignerWit
  *
  * NOTE: Add networks as needed
  */
-export type DeployableNetworks = Extract<Networks, 'bsc' | 'polygon' | 'bscTestnet'>
+export type DeployableNetworks = Extract<Networks, 'bsc' | 'polygon' /*| 'bscTestnet'*/>
 
 /**
  * Deployment Variables for each network
@@ -31,18 +33,15 @@ export type DeployableNetworks = Extract<Networks, 'bsc' | 'polygon' | 'bscTestn
  * NOTE: Update variables as needed
  */
 interface DeploymentVariables {
-  proxyAdminAddress: string
-  adminAddress: string | SignerWithAddress
-  admin: string
+  adminAddress: string
   wNative: string
   dexInfo: Partial<Record<Dex, { factory: string; router: string; hopTokens: string[] }>>
   feeCollector: string
-  protocolFee: number
-  maxFee: number
   soulFeeManager?: string
+  feeTokens: string[]
   soulAccessRegistry?: string
   SoulZap_UniV2?: string
-  volumesAndFees: { volumes: number[]; fees: number[] }
+  volumesAndFees: { volumes: BigNumberish[]; fees: BigNumberish[] }
 }
 
 export const enum Dex {
@@ -51,13 +50,17 @@ export const enum Dex {
   QuickSwap = 'QuickSwap',
 }
 
+const create2Addresses = {
+  soulAccessRegistry: '0x2433594aC4736DE4898e6cd1DF74e1d301132b1C',
+  feeCollector: '0x3cA81D787a58100C85465F798086BF632d3f1534',
+}
+
 const deployableNetworkConfig: Record<DeployableNetworks, (signers?: SignerWithAddress[]) => DeploymentVariables> = {
   bsc: (signers?: SignerWithAddress[]) => {
     return {
-      proxyAdminAddress: '0x',
-      // NOTE: Example of extracting signers
-      adminAddress: signers?.[0] || '0x',
-      admin: '0x5c7C7246bD8a18DF5f6Ee422f9F8CCDF716A6aD2',
+      soulAccessRegistry: create2Addresses.soulAccessRegistry, // CREATE2 Same address on bsc & polygon
+      adminAddress: '', // Leaving blank in place of registry
+      // admin: '0x5c7C7246bD8a18DF5f6Ee422f9F8CCDF716A6aD2',
       wNative: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
       dexInfo: {
         [Dex.ApeBond]: {
@@ -85,21 +88,23 @@ const deployableNetworkConfig: Record<DeployableNetworks, (signers?: SignerWithA
           ],
         },
       },
-      feeCollector: '0x5c7C7246bD8a18DF5f6Ee422f9F8CCDF716A6aD2',
-      protocolFee: 300,
-      maxFee: 1000,
-      soulFeeManager: '0x',
-      soulAccessRegistry: '0x',
-      SoulZap_UniV2: '0x',
-      volumesAndFees: { volumes: [0], fees: [100] },
+      feeCollector: create2Addresses.feeCollector, // CREATE2 Same address on bsc & polygon
+      feeTokens: [
+        '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d', //USDC
+        '0x55d398326f99059fF775485246999027B3197955', //USDT
+        '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', //DAI
+      ],
+      // soulFeeManager: '', // NOTE: Deployed 2023.12.06 with USDC, USDT, DAI
+      // SoulZap_UniV2: '',
+      // NOTE: ApeBond Factory must be whitelisted to skip fee taking:
+      volumesAndFees: { volumes: [ether(0), ether(100_000), ether(300_000)], fees: [100, 50, 20] },
     }
   },
   polygon: (signers?: SignerWithAddress[]) => {
     return {
-      proxyAdminAddress: '0x',
       // NOTE: Example of extracting signers
-      adminAddress: signers?.[0] || '0x',
-      admin: '0x5c7C7246bD8a18DF5f6Ee422f9F8CCDF716A6aD2',
+      soulAccessRegistry: create2Addresses.soulAccessRegistry, // CREATE2 Same address on bsc & polygon
+      adminAddress: '', // Leaving blank in place of registry
       wNative: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
       dexInfo: {
         [Dex.ApeBond]: {
@@ -127,16 +132,20 @@ const deployableNetworkConfig: Record<DeployableNetworks, (signers?: SignerWithA
           ],
         },
       },
-      feeCollector: '0x5c7C7246bD8a18DF5f6Ee422f9F8CCDF716A6aD2',
-      protocolFee: 300,
-      maxFee: 1000,
-      soulFeeManager: '0x5A79F4446ff39e9519F379A99184cFb43C641674', //'0x832FAa0244eE3972473b5d7DA708972d50fF3476',
-      soulAccessRegistry: '0x61B428C02CB1058F9AAf2bFeACCB00333c50E0A1', //'0xDe8b10E44A099d28A8991F5b678D3F991CD4566d',
-      SoulZap_UniV2: '0xDD20343AeB210f5b7Ec9Db7B3727F8D0a8070c42', //0xf95A84d25Af7575110489566d85827e28108c80a',
-      volumesAndFees: { volumes: [0], fees: [100] },
+      feeCollector: create2Addresses.feeCollector, // CREATE2 Same address on bsc & polygon
+      feeTokens: [
+        '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', //USDC
+        '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', //USDT
+        '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', //DAI
+      ],
+      soulFeeManager: '0xFe50E927d74e6bc65cD07a9c68Bb7b44cbff0466', // NOTE: Deployed 2023.12.06 with USDC, USDT, DAI
+      SoulZap_UniV2: '0x133141571DC83783d7c05138af8aA9cc2189c1A7',
+      // NOTE: ApeBond Factory must be whitelisted to skip fee taking:
+      volumesAndFees: { volumes: [ether(0), ether(100_000), ether(300_000)], fees: [100, 50, 20] }, // 1%, .5%, .2%
       //lens: 0xA400A9a00bd1b7ca90BbC5F8DB0d3d723da8D72c
     }
   },
+  /*
   bscTestnet: (signers?: SignerWithAddress[]) => {
     return {
       proxyAdminAddress: '0x',
@@ -159,4 +168,5 @@ const deployableNetworkConfig: Record<DeployableNetworks, (signers?: SignerWithA
       volumesAndFees: { volumes: [0], fees: [100] },
     }
   },
+  */
 }
--- scripts/deploy/deployZap.ts
+++ scripts/deploy/deployZap.ts
@@ -1,6 +1,8 @@
 import { ethers, network } from 'hardhat'
 import { getDeployConfig, DeployableNetworks } from './deploy.config'
 import { DeployManager } from './DeployManager'
+import { writeObjectToTsFile } from '../utils/files'
+import { convertAddressesToExplorerLinksByNetwork, logger } from '../../hardhat/utils'
 
 /**
  * // NOTE: This is an example of the default hardhat deployment approach.
@@ -11,68 +13,74 @@ async function main() {
   const currentNetwork = network.name as DeployableNetworks
   // Optionally pass in accounts to be able to use them in the deployConfig
   const accounts = await ethers.getSigners()
-  const {
-    wNative,
-    admin,
-    dexInfo,
-    feeCollector,
-    soulFeeManager,
-    soulAccessRegistry,
-    protocolFee,
-    maxFee,
-    volumesAndFees,
-  } = getDeployConfig(currentNetwork, accounts)
+  const deployConfig = getDeployConfig(currentNetwork, accounts)
+  const { wNative, adminAddress, feeCollector, soulFeeManager, soulAccessRegistry, volumesAndFees, feeTokens } =
+    deployConfig
   // Optionally pass in signer to deploy contracts
   const deployManager = await DeployManager.create(accounts[0])
 
   let soulAccessRegistryAddress = soulAccessRegistry
   if (!soulAccessRegistry || soulAccessRegistry == '0x') {
+    logger.log(`Deploying SoulAccessRegistry...`, '🔧')
     const SoulAccessRegistry = 'SoulAccessRegistry'
     const SoulAccessRegistryContract = await ethers.getContractFactory(SoulAccessRegistry)
     const soulAccessRegistryContract = await deployManager.deployContractFromFactory(
       SoulAccessRegistryContract,
-      [true],
+      [],
       SoulAccessRegistry
     )
-    await soulAccessRegistryContract.initialize(admin)
+    await soulAccessRegistryContract.initialize(adminAddress)
     soulAccessRegistryAddress = soulAccessRegistryContract.address
+  } else {
+    logger.log(`Using existing SoulAccessRegistry at ${soulAccessRegistryAddress}`, '🔄')
   }
-  console.log('SoulAccessManager contract at:', soulAccessRegistryAddress)
 
   let soulFeeManagerAddress = soulFeeManager
   if (!soulFeeManager || soulFeeManager == '0x') {
+    logger.log(`Deploying soulFeeManager...`, '🔧')
     const SoulFeeManager = 'SoulFeeManager'
     const SoulFeeManagerContract = await ethers.getContractFactory(SoulFeeManager)
     const soulFeeManagerContract = await deployManager.deployContractFromFactory(
       SoulFeeManagerContract,
-      [
-        soulAccessRegistryAddress!,
-        [dexInfo.ApeBond?.hopTokens[0]!],
-        feeCollector,
-        volumesAndFees.volumes,
-        volumesAndFees.fees,
-      ],
+      [soulAccessRegistryAddress!, feeTokens, feeCollector, volumesAndFees.volumes, volumesAndFees.fees],
       SoulFeeManager
     )
     soulFeeManagerAddress = soulFeeManagerContract.address
+  } else {
+    logger.log(`Using existing SoulFeeManager at ${soulFeeManagerAddress}`, '🔄')
   }
-  console.log('SoulFeeManager contract at:', soulFeeManagerAddress)
 
-  const SoulZap_UniV2 = 'SoulZap_UniV2_Extended_V1'
-  const RoutingContract = await ethers.getContractFactory(SoulZap_UniV2)
-  const routingContract = await deployManager.deployContractFromFactory(
-    RoutingContract,
+  await delay(20000)
+
+  const SoulZap_UniV2_Extended_V1_name = 'SoulZap_UniV2_Extended_V1'
+  const SoulZap_UniV2_Extended_V1 = await ethers.getContractFactory(SoulZap_UniV2_Extended_V1_name)
+  const soulZap_UniV2_Extended_V1 = await deployManager.deployContractFromFactory(
+    SoulZap_UniV2_Extended_V1,
     [soulAccessRegistryAddress!, wNative, soulFeeManagerAddress!, 0],
-    SoulZap_UniV2 // Pass in contract name to log contract
+    SoulZap_UniV2_Extended_V1_name
   )
-  console.log('SoulZap_UniV2 contract deployed at:', routingContract.address)
+  console.log('SoulZap_UniV2 contract deployed at:', soulZap_UniV2_Extended_V1.address)
+
+  const output = convertAddressesToExplorerLinksByNetwork(
+    {
+      SoulZap_UniV2_Extended_V1: soulZap_UniV2_Extended_V1.address,
+      deployConfig,
+      NEXT_STEPS: {
+        step0: 'Whitelist router contracts',
+        step1: 'Whitelist bond contracts',
+      },
+    },
+    currentNetwork
+  )
+  console.dir(output, { depth: 5 })
+  await writeObjectToTsFile(__dirname + `/deployment-${currentNetwork}.ts`, 'deployment', output)
 
   await delay(20000)
   // await deployManager.addDeployedContract('20231122-polygon-deployment.json')
   await deployManager.verifyContracts()
 }
 
-function delay(ms: number) {
+export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
